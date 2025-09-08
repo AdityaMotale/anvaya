21f40091d32b9cfc524773bd263b72ae9b0444c6
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-09-08 19:45:34 +0530

    (viccheda) impl of `vyanjan-chatrva` rules (1 sub-rule)

--- viccheda/src/rules/vyanjan/chatrva.rs
+++ viccheda/src/rules/vyanjan/chatrva.rs
@@ -1,5 +1,5 @@
 use crate::rules::{
-    rule::{BaseRule, RuleData, RuleGroup},
+    rule::{BaseRule, MultiOptRule, RuleData, RuleGroup},
     Rule,
 };
 use orthography::{Adjuncts, Akshara, Consonant, SoundClass, Vowel};
@@ -18,21 +18,58 @@ impl RuleGroup for VynjanChatrva {
 
 impl VynjanChatrva {
     fn char_to_zal_khal() -> Vec<Box<dyn Rule>> {
-        vec![Box::new(BaseRule(RuleData {
-            name: "vyanjan-chatrva-char1",
-            desc: "त् = द् + अ ",
-            tag: "8.4.44",
-            left: Akshara(vec![
-                SoundClass::Consonant(Consonant::Da),
-                SoundClass::Adjuncts(Adjuncts::VIRAMA),
-            ]),
-            right: Akshara(vec![SoundClass::Vowel(Vowel::A)]),
-            merged: Akshara(vec![
-                SoundClass::Consonant(Consonant::Ta),
-                SoundClass::Adjuncts(Adjuncts::VIRAMA),
-            ]),
-            special_sequence: None,
-        }))]
+        let merge_items: [Consonant; 5] = [
+            Consonant::Ta,
+            Consonant::Pa,
+            Consonant::Cha,
+            Consonant::Ka,
+            Consonant::Tta,
+        ];
+
+        let swap_items: [Consonant; 5] = [
+            Consonant::Da,
+            Consonant::Bha,
+            Consonant::Da,
+            Consonant::Ga,
+            Consonant::Dda,
+        ];
+
+        let merged_list: Vec<Akshara> = merge_items
+            .iter()
+            .map(|consonant| Akshara(vec![SoundClass::Consonant(*consonant)]))
+            .collect();
+
+        let swap_list: Vec<Akshara> = swap_items
+            .iter()
+            .map(|consonant| {
+                Akshara(vec![
+                    SoundClass::Consonant(*consonant),
+                    SoundClass::Adjuncts(Adjuncts::VIRAMA),
+                ])
+            })
+            .collect();
+
+        assert!(merged_list.len() == swap_list.len());
+
+        vec![Box::new(MultiOptRule {
+            merged_list,
+            swap_list,
+            data: RuleData {
+                name: "vyanjan-chatrva",
+                desc: "क्, च्, ट्, त्, प् = द्, भ्, द्, ग्, ड् + अ ",
+                tag: "8.4.44",
+                left: Akshara(vec![]),
+                right: Akshara(vec![SoundClass::Vowel(Vowel::A)]),
+                merged: Akshara(vec![]),
+                special_sequence: Some(vec![
+                    (
+                        Akshara(vec![SoundClass::Adjuncts(Adjuncts::ANUSVARA)]),
+                        false,
+                    ),
+                    (Akshara(vec![SoundClass::Adjuncts(Adjuncts::VIRAMA)]), false),
+                ]),
+            },
+        })]
     }
 }
 
@@ -45,7 +82,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn char_to_zal_khal_debug() {
         create_logger();
         let cases: Vec<(&str, Vec<Vec<&str>>)> = vec![("सत्कारः", vec![vec!["सद्", "कारः"]])];
@@ -53,7 +89,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn char_to_zal_khal_test() {
         let cases: Vec<(&str, Vec<Vec<&str>>)> = vec![
             ("सत्कारः", vec![vec!["सद्", "कारः"]]),
@@ -66,18 +101,15 @@ mod tests {
             ("उत्कर्षः", vec![vec!["उद्", "कर्षः"]]),
             ("उत्कीर्णः", vec![vec!["उद्", "कीर्णः"]]),
             ("उत्पत्तिः", vec![vec!["उद्", "पत्तिः"]]),
-            // ("उत्थानम्", vec![vec!["उद्", "स्थानम्"]]),
             ("तत्क्षणः", vec![vec!["तद्", "क्षणः"]]),
             ("तत्परः", vec![vec!["तद्", "परः"]]),
             ("तत्पुरुषः", vec![vec!["तद्", "पुरुषः"]]),
             ("तच्छविः", vec![vec!["तद्", "छविः"]]),
-            ("तच्छिवः", vec![vec!["तद्", "शिवः"]]),
             ("संसत्सदस्यः", vec![vec!["संसद्", "सदस्यः"]]),
             ("आपत्तिः", vec![vec!["आपद्", "तिः"]]),
             ("लप्स्यते", vec![vec!["लभ्", "स्यते"]]),
             ("दिक्पालः", vec![vec!["दिग्", "पालः"]]),
             ("अस्मत्पुत्रः", vec![vec!["अस्मद्", "पुत्रः"]]),
-            ("विराट्पुरुषः", vec![vec!["विराड्", "षुरुषः"]]),
             ("षट्खाद्यानि", vec![vec!["षड्", "खाद्यानि"]]),
             ("भेत्तुम्", vec![vec!["भेद्", "तुम्"]]),
         ];
